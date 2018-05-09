405f093cd5cbea68ee33dc968124ab217e95f1ee
Saulo Mendes Martins <saulo@ip-192-168-0-106.ec2.internal>
2018-05-09 17:28:59 -0300

    Add validate data for AlfaNumeric

--- www/urbem/tests/Feature/CampoTcePrAlfanumericoTest.php
+++ www/urbem/tests/Feature/CampoTcePrAlfanumericoTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Mockery\Exception;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,35 +14,36 @@ class CampoTcePrAlfanumericoTest extends TestCase
 	 */
 	private $service;
 
-//	function setUp()
-//	{
-//		parent::setUp();
-//		$this->service = \Illuminate\Support\Facades\App::make(\App\Services\CampoTcePrAlfanumericoService::class);
-//		$this->service->setNome("Nome da Pessoa");
-//	}
-//
-//	/**
-//	 * A basic test on CampoTcePrAlfanumerico.
-//	 *
-//	 * @return void
-//	 */
-//	public function testCampoObrigatorio()
-//	{
-//		$this->service->setObritarorio(true);
-//		$this->service->setValor(null);
-//		$this->getConteudo();
-//		$this->expectException("Nome da Pessoa é obrigatório!");
-//	}
-//
-//	/**
-//	 * A basic test on CampoTcePrData.
-//	 *
-//	 * @return void
-//	 */
-//	public function testCampoAlphanumericoOk()
-//	{
-//		$this->service->setTamanho(10);
-//		$this->service->setValor("Jõao da Silva");
-//		$this->assertTrue($this->service->getConteudo() == "Jõao da Si");
-//	}
+	function setUp()
+	{
+		parent::setUp();
+		$this->service = \Illuminate\Support\Facades\App::make(\App\Services\CampoTcePrAlfanumericoService::class);
+		$this->service->setNome("Nome da Pessoa");
+	}
+
+	/**
+	 * A basic test on CampoTcePrAlfanumerico.
+	 *
+	 * @return void
+	 */
+	public function testCampoObrigatorio()
+	{
+        $this->expectException(Exception::class);
+		$this->service->setObrigatorio(true);
+		$this->service->setValor('');
+		$this->service->getConteudo();
+		$this->expectException("Nome da Pessoa é obrigatório!");
+	}
+
+	/**
+	 * A basic test on CampoTcePrData.
+	 *
+	 * @return void
+	 */
+	public function testCampoAlphanumericoOk()
+	{
+		$this->service->setTamanho([10]);
+		$this->service->setValor("Jõao da Silva");
+        $this->assertSame('Jõao da Si', $this->service->getConteudo(),"string");
+	}
 }
