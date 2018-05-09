0bca1160922eaff8d46fbcd7f9ae8bfe6dfc2e24
Saulo Mendes Martins <saulommartins@gmail.com>
2018-05-09 09:07:03 -0300

    Add validate data for CampoData

--- www/urbem/app/Services/CampoTcePrDataService.php
+++ www/urbem/app/Services/CampoTcePrDataService.php
@@ -10,6 +10,11 @@ namespace App\Services;
 
 use Mockery\Exception;
 
+/**
+ * Class CampoTcePrDataService
+ *
+ * @package App\Services
+ */
 class CampoTcePrDataService extends \App\Services\CampoService {
 
 	/**
@@ -26,7 +31,9 @@ class CampoTcePrDataService extends \App\Services\CampoService {
 	}
 
 	/**
-	 * @param array $valor
+	 * @param int $dia
+	 * @param int $mes
+	 * @param int $ano
 	 */
 	public function setData(int $dia, int $mes, int $ano) {
 		$this->data = [];
@@ -34,14 +41,18 @@ class CampoTcePrDataService extends \App\Services\CampoService {
 		$this->data['m'] = $mes;
 		$this->data['y'] = $ano;
 	}
+
 	public function preparaConteudo() : void {
-		$conteudo = $this->getData();
-		if (empty($conteudo)) {
+		$valor = $this->getData();
+		if (empty($valor)) {
 			throw new Exception('Campo ' . $this->getNome() . ' é obrigatório!');
 		}
-		$conteudo = str_pad($conteudo['y'],4, 0, STR_PAD_LEFT)."-".str_pad($conteudo['m'],2,
-				0,
-				STR_PAD_LEFT)."-".str_pad($conteudo['d'],2, 0, STR_PAD_LEFT);
+		$ano = str_pad($valor['y'],4, 0, STR_PAD_LEFT);
+		$mes = str_pad($valor['m'],2,0,STR_PAD_LEFT);
+		$dia = str_pad($valor['d'],2, 0,STR_PAD_LEFT);
+		$conteudo = str_replace("AAAA", $ano, $this->getFormato());
+		$conteudo = str_replace("MM", $mes, $conteudo);
+		$conteudo = str_replace("DD", $dia, $conteudo);
 		$this->setConteudo($conteudo);
 	}
 
--- www/urbem/tests/Feature/CampoTcePrDataTest.php
+++ www/urbem/tests/Feature/CampoTcePrDataTest.php
@@ -34,7 +34,6 @@ class CampoTcePrDataTest extends TestCase
 		$this->service->getConteudo();
 	}
 
-
 	/**
 	 * A basic test on CampoTcePrData.
 	 *
@@ -46,5 +45,18 @@ class CampoTcePrDataTest extends TestCase
 		$this->service->setFormato("AAAA-MM-DD");
 		$this->service->setData(5,10,2017);
 		$this->assertEquals($this->service->getConteudo(),"2017-10-05");
+	}
+
+	/**
+	 * A basic test on CampoTcePrData.
+	 *
+	 * @return void
+	 */
+	public function testCampoDataFormato()
+	{
+		$this->service->setObrigatorio(false);
+		$this->service->setFormato("DD-MM-AAAA");
+		$this->service->setData(5,10,2017);
+		$this->assertEquals($this->service->getConteudo(),"05-10-2017");
 	}
 }
